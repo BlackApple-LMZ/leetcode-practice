50abf07251ee834bff49de9ac522796ab567ce7e
wuliyanyan <limz1994@126.com>
2019-04-12 22:28:08 +0800

    Update 2-Add Two Numbers.cpp

--- LinkedList/2-Add Two Numbers.cpp
+++ LinkedList/2-Add Two Numbers.cpp
@@ -4,27 +4,24 @@ struct ListNode {
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
 
-ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) 
-{ 
-    ListNode *p=new ListNode(-1); 
-    ListNode *q=p; 
-    int carry=0; 
-    while(l1||l2) { 
-        int val1= l1?l1->val:0;//为空时做加法的位数为0 
-        int val2= l2?l2->val:0; 
-        int val3=val1+val2+carry; 
-        if(val3>=10) { 
-            val3=val3-10; 
-            carry=1; 
-        } 
-        else 
-            carry=0; 
-        q->next=new ListNode(val3);//创建新节点 
-        q=q->next; 
-        l1=l1?l1->next:l1; 
-        l2=l2?l2->next:l2;//当l1和l2为null时不取其next指针，一直让其保持空的状态 
-    } 
-    if(carry==1)//考虑最高位进位的情况 
-        q->next=new ListNode(1); 
-    return p->next; 
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    int add(0);
+    ListNode node(0);
+    ListNode* temp = &node;
+    while(l1 || l2){
+        int x = l1?l1->val:0;
+        int y = l2?l2->val:0;
+        int sum = x + y + add;
+        add = sum/10;
+
+        temp->next = new ListNode(sum % 10);
+        temp = temp->next;
+
+        if(l1) l1 = l1->next;
+        if(l2) l2 = l2->next;
+    }
+    if(add){
+        temp->next = new ListNode(1);
+    }
+    return node.next;
 }

113f9cdfaed7ad375114367102f726ffdfed6300
wuliyanyan <limz1994@126.com>
2019-04-13 17:38:24 +0800

    Update 46-Permutations.cpp

--- Backtracking/46-Permutations.cpp
+++ Backtracking/46-Permutations.cpp
@@ -15,3 +15,29 @@ vector<vector<int>> permute(vector<int>& nums) {
     permutation(nums, 0, nums.size() - 1, result);
     return result;
 }
+//方法二 加一个used数组来判断
+vector<vector<int>> permute(vector<int>& nums) {
+    vector<vector<int>> res;
+    vector<int> temp;
+    vector<int> used(nums.size(), 0);
+    help(nums, res, temp, used);
+    return res;
+}
+
+void help(vector<int>& nums, vector<vector<int>>& res, vector<int>& temp, vector<int>& used){
+    if(temp.size() == nums.size()){
+        res.push_back(temp);
+        return ;
+    }
+    for(int i=0; i<nums.size(); i++){
+        if(!used[i]){
+            cout<<i<<" ";
+            used[i] = 1;
+            temp.push_back(nums[i]);
+            help(nums, res, temp, used);
+            used[i] = 0;
+            cout<<i<<" ";
+            temp.pop_back();
+        }
+    }
+}

33eaf8ec7f25074bffeed4c170893798fa821ac2
wuliyanyan <limz1994@126.com>
2019-04-22 21:07:15 +0800

    Update 136-Single Number.cpp

--- HashTable/136-Single Number.cpp
+++ HashTable/136-Single Number.cpp
@@ -17,3 +17,14 @@ int singleNumber(vector<int>& nums) {
     }
     return 0;
 }
+int singleNumber(vector<int>& nums) {
+    unordered_set<int> s;
+    for(int num : nums)
+    {
+        if(s.count(num))
+            s.erase(num);
+        else
+            s.insert(num);
+    }
+    return *s.begin();
+}

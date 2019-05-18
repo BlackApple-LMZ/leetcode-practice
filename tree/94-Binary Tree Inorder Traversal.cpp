845e970b73e48b403121890b74e82a34185a13ff
wuliyanyan <limz1994@126.com>
2019-05-18 20:48:05 +0800

    Update 94-Binary Tree Inorder Traversal.cpp

--- tree/94-Binary Tree Inorder Traversal.cpp
+++ tree/94-Binary Tree Inorder Traversal.cpp
@@ -1,3 +1,4 @@
+//Recursive 
 void helper(TreeNode* root,  vector<int>& res) {
     if(root){
         helper(root->left, res);
@@ -9,5 +10,45 @@ void helper(TreeNode* root,  vector<int>& res) {
 vector<int> inorderTraversal(TreeNode* root) {
     vector<int> res;
     helper(root, res);
+    return res;
+}
+//iterative
+vector<int> inorderTraversal(TreeNode* root) {
+    stack<TreeNode*> roo;
+    vector<int> res;
+    TreeNode* cur = root;
+    while(cur || !roo.empty()){
+        while(cur){
+            roo.push(cur);
+            cur = cur->left;
+        }
+        cur = roo.top();
+        roo.pop();
+        res.push_back(cur->val);
+        cur = cur->right;
+    }
+    return res;
+}
+//Morris Traversal
+vector<int> inorderTraversal(TreeNode* root) {
+    vector<int> res;
+    TreeNode* cur = root;
+    TreeNode* pre;
+    while(cur){
+        if(!cur->left){
+            res.push_back(cur->val);
+            cur = cur->right;
+        }
+        else{
+            TreeNode* temp = cur->left;
+            pre = cur->left;
+            while(pre->right){
+                pre = pre->right;
+            }
+            pre->right = cur;
+            cur->left = NULL;
+            cur = temp;
+        }
+    }
     return res;
 }
